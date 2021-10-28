0d951579f9232aa7bb40af3e403f60ca550c6547
mustayde <35762106+mustayde@users.noreply.github.com>
2021-10-28 13:26:10 +0200

    Add files via upload

---
+++ angular-chapter5-master_new version/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `    
+    <products></products>                            
+    `
+})
+export class AppComponent {
+}
+
+// <products></products>
